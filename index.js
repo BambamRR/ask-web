1789970e9475ba3a03f367879f9b07adf78fe34c
BambamRR <leopm.jus@gmail.com>
2022-09-13 03:13:54 -0300

    all frontend files up-to-date

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.listen(8080, () => console.log("App rodando"));
 //sayng to express to use EJS to renderize html pages
@@ -7,10 +8,15 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 
-app.get("/", (req, res) =>{
-
-
-        res.render("index");
-
+app.get("/", (req, res) => {
+    res.render("index");
 });
+
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar");
+})
+
+app.post("/salvarpergunta", (req, res) => {
+    res.send("Formulário Recebido");
+})
 
