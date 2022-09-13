d12c7a49c3dfa7bda238b0be5449cac37b372890
BambamRR <leopm.jus@gmail.com>
2022-09-13 09:43:29 -0300

    Back end files with dependencies for database using sequelize and mysql2

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const connection = require("./databse/database");
+const Pergunta = require("./databse/Pergunta");
+//database
+
+connection
+    .authenticate()
+    .then(() => console.log("Conection with database success"))
+    .catch((err) => {
+        console.log(err);
+    })
+    ;
 
 app.listen(8080, () => console.log("App rodando"));
 //sayng to express to use EJS to renderize html pages
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+//Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-
+//ROUTES
 app.get("/", (req, res) => {
-    res.render("index");
+    Pergunta.findAll({ raw: true}).then(perguntas => {
+        res.render("index", {
+            perguntas: perguntas
+        });
+    });
+   
 });
 
 app.get("/perguntar", (req, res) => {
@@ -17,6 +36,15 @@ app.get("/perguntar", (req, res) => {
 })
 
 app.post("/salvarpergunta", (req, res) => {
-    res.send("Formulário Recebido");
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(() => {
+        res.redirect("/");
+    });
 })
+
+app.get
 
